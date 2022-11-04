53b4f20ef97e2e2b85ce68dc93ed02052b59a37c
Hmilner1 <72082549+Hmilner1@users.noreply.github.com>
2022-11-04 04:34:08 +0000

    Gamemode Update
    Gamemode Updated to open on the correct scene

--- DialogueSystem/Intermediate/Build/Win64/DialogueSystemEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ DialogueSystem/Intermediate/Build/Win64/DialogueSystemEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.29.30146
-#include "E:/Uni Year 3/Mechanics Programming/Mechanics Programming/DialogueSystem/Intermediate/Build/Win64/DialogueSystemEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "E:/Uni Year 3/Mechanics Programming/MechanicsProgramming/MechanicsProgramming/DialogueSystem/Intermediate/Build/Win64/DialogueSystemEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ DialogueSystem/Intermediate/Build/Win64/DialogueSystemEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "E:/Uni Year 3/Mechanics Programming/Mechanics Programming/DialogueSystem/Intermediate/Build/Win64/DialogueSystemEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
